486e1169f2b1acb8071fa1becbf01cdd0e966408
Vamsi Arikatla <arikatlavamsi478@gmail.com>
2021-03-05 18:27:41 -0800

    Variables

---
+++ Typescript_Examples/Variables.js
@@ -0,0 +1,14 @@
+var sub = "ReactJs";
+console.log(sub);
+var my_sub = "ReactJs With Typescript";
+var wish = "Welcome to " + my_sub;
+console.log(wish);
+var table_name = "employees";
+var salalry = 70000;
+var sql_query = "select * from"+ table_name + " where emp_sal>=" + salalry;
+console.log(sql_query);
+var login_table = "user_table";
+var user_name = "admin";
+var user_password = "Vamschakri#143";
+var login_query = "SELECT * FROM "+ login_table + " WHERE USER_NAME " + user_name + " AND USER_PASSWORD " + user_password;
+console.log(login_query);
